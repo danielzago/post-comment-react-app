2bfcf1696ad8f8f373428ad08326bb88726c3f75
danielzago <danielaugustozago@gmail.com>
2025-03-05 19:19:33 -0300

    First Scenerio cypress automation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,13 +2,15 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
-    cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
-    cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
-        cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
-      }
+    cy.get('[data-qa="comment-input"]').first().type('teste 123')
+    cy.get("[data-qa='comment-input']")
+    cy.get("[data-qa='publish-button']").first().click()
+    cy.get("[data-qa='comment-text']").contains('teste 123')
+    cy.get("[data-qa='comment-text']").each(($ellement) => {
+     cy.log($ellement.text())
+     if($ellement.text()=== 'teste 123'){
+      cy.get($ellement).siblings('header').children('[data-qa="delete-button"]').click()
+     }
     })
   })
 })
